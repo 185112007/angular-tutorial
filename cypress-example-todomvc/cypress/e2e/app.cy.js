3c2aa32399f032b4fa2901fab23e4aa8892accb4
185112007 <185112007@kocaeli.edu.tr>
2022-08-19 13:14:59 +0300

    using .then(), .wrap(), .as() & .wait()

--- cypress-example-todomvc/cypress/e2e/app.cy.js
+++ cypress-example-todomvc/cypress/e2e/app.cy.js
@@ -17,4 +17,31 @@ describe('React - TodoMVC', () => {
     cy.get(".todo-list li").should("have.length", 1)
   })
 
+  // using .then() & .wrap()
+  describe("using .then() & .wrap()", ()=>{
+    it("getting the <button> HTML element.", ()=>{
+      cy.get("button").then(($btn) => {
+        const cls = $btn.attr("class")
+      
+        cy.wrap($btn).click().should("not.have.class", cls)
+      })
+    })
+  })
+
+  // using alias & waiting
+  describe("User Sign-up and Login", ()=>{
+    beforeEach(() => {
+      cy.request("POST", "/users").as("signup") // creating the signup alias
+    })
+  
+    it.skip("should allow a visitor to sign-up, login, and logout", () => {
+      cy.visit("/")
+      // ...
+  
+      cy.wait("@signup") // waiting upon the signup alias
+  
+      // ...
+    })
+  })
+
 })
