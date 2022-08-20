c6d1d4f058d550f18246cf4d5f2b45483943d81a
185112007 <185112007@kocaeli.edu.tr>
2022-08-20 23:16:37 +0300

    reusing steps

--- cy-bdd/cypress/integration/common/common.steps.spec.js
+++ cy-bdd/cypress/integration/common/common.steps.spec.js
@@ -4,6 +4,27 @@ Given("I launch the SwagLab site", () => {
     cy.main();
 });
 
+And("I insert the {string} username", (userType) => {
+
+    cy.fixture('selectors').then((el) => {
+        switch (userType){
+            case "standard":
+                cy.username(el.standardUser);
+                break;
+            case "problem":
+                cy.username(el.problemUser);
+                break;
+            case "locked":
+                cy.username(el.lockOutUser);
+                break;
+            case "performance":
+                cy.username(el.performanceGlitchUser);
+                break;
+        }
+    })
+
+});
+
 And("I insert the password", () => {
     cy.password("secret_sauce");
 })
