9d69a3779631dd5ab2e4efa498c427b63a0947ce
Gafur Hayybayev <185112007@kocaeli.edu.tr>
2022-10-04 23:36:38 +0300

    testing component with service dependencies

--- workspace/part-01/simple-angular-app/src/app/page1/page1.component.spec.ts
+++ workspace/part-01/simple-angular-app/src/app/page1/page1.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Page1Component } from './page1.component';
+import {Book} from "../model/Book";
+import {DataService} from "../data.service";
 
 describe('Page1Component', () => {
   let component: Page1Component;
@@ -19,5 +21,29 @@ describe('Page1Component', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('number of books written by matt is incremented ', () => {
+    const startedValue = component.numberOfBookWrittenByMatt;
+    const book = new Book();
+    book.author = 'matt';
+
+    const dataService = fixture.debugElement.injector.get(DataService);
+
+    dataService.addBook(book);
+    expect(component.numberOfBookWrittenByMatt).toEqual(startedValue + 1);
+  });
+
+  it('number of books written by matt is incremented version 2', () => {
+    const startedValue = component.numberOfBookWrittenByMatt;
+    const book = new Book();
+    book.author = 'matt';
+
+    const dataService = new DataService();
+    component = new Page1Component(dataService);
+    component.ngOnInit();
+
+    dataService.addBook(book);
+    expect(component.numberOfBookWrittenByMatt).toEqual(startedValue + 1);
   });
 });
